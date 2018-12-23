1cc93d8d6c93e5ae45f8ec453be6a50e08a4ed21
Admin <admin@Admins-MacBook-Pro.local>
2018-12-24 00:20:54 +0800

    using createReadStream and createInterface to read each line of source.txt file

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-const fs = require('fs');
+const path = require('path');
+const reader = require('./functions-library/reader');
 
-fs.readFile('resources/source.txt', 'utf8', function(err, contents) {
-    var allTalksArr = contents.split("\n")
-    allTalksArr.forEach(function(line, index, arr) {
-        if (index === arr.length - 1 && line === "") {
-            return;
-        }
-        console.log(index + " " + line);
-    });
+let util = {};
+
+util.path = {
+    getRightPath: paths => {
+        let rightPath = [];
+
+        paths.forEach(_path => {
+            let tmp = path.relative(__dirname, _path);
+            rightPath.push(path.join(__dirname, tmp));
+        });
+
+        return rightPath;
+    }
+};
+
+let argv = process.argv.slice(2);
+
+// Get the file content and do further processing
+let files = util.path.getRightPath(argv);
+reader.getTalkList(files).then(talkList => {
+    
+    console.log("talkList", talkList);
+
+}).catch(err => {
+    // Handling Errors
+    console.log(`Error[${err}]`);
 });
