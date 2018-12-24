449c1d4025f8a167fe9ced1ba2a18a1d9368d944
Admin <admin@Admins-MacBook-Pro.local>
2018-12-24 11:35:54 +0800

    implement track generator function to generate an empty schedule

--- app.js
+++ app.js
@@ -24,6 +24,81 @@ if (idx != -1) {
 
 global.config = require(cfg);
 
+
+let track = {};
+let time = {};
+
+// Convert string time types into structured time types for later operations
+let str2Obj = str => {
+    let tmp = str.split(':');
+    let obj = {};
+
+    obj.hr = Number(tmp[0]);
+    obj.min = Number(tmp[1]);
+
+    if (obj.hr > 23 || obj.min > 59 || obj.hr < 0 || obj.min < 0) {
+        throw new Error('Error[Illegal time]' + ` Hour[${obj.hr}]` + ` Minute[${obj.min}]`);
+    }
+
+    return obj;
+}
+
+// Calculate the number of minutes between two time points, such as 15 minutes between 18:00 and 18:15
+time.duration = (begin, end) => {
+    let oBegin = str2Obj(begin);
+    let oEnd = str2Obj(end);
+    let beginMinOffset;
+    let endMinOffset;
+
+    if ((oBegin.hr > oEnd.hr) || (oBegin.hr === oEnd.hr && oBegin.min > oEnd.min)) {
+        throw new Error('Error[Illegal time]' + ` Start[${begin}]` + ` End[${end}]`);
+    }
+
+    if (oBegin.min === 0) {
+        beginMinOffset = 0;
+    } else {
+        oBegin.hr += 1;
+        beginMinOffset = 60 - oBegin.min;
+    }
+    if (oEnd.min === 0) {
+        endMinOffset = 0;
+    } else {
+        oEnd.hr += 1;
+        endMinOffset = 60 - oEnd.min;
+    }
+
+    return ((oEnd.hr - oBegin.hr) * 60) + beginMinOffset - endMinOffset;
+}
+
+// Generate an empty schedule
+track.generator = theXDay => {
+    this.track = {};
+    this.track.sessions = [];
+    this.track.timeUsed = 0;
+    this.track.timeRemain = 0;
+
+    let section = (theXDay ? global.config.session.section[theXDay] : false) || global.config.session.section.default;
+
+    section.forEach(item => {
+        let session = {};
+
+        item = item.split(global.config.session.section.split);
+        session.begin = item[0];
+        session.end = item[1];
+        session.finish = item[2];
+        session.timeUsed = 0;
+        session.timeRemain = time.duration(session.begin, session.end);
+        session.talks = [];
+
+        this.track.timeRemain += session.timeRemain;
+
+        this.track.sessions.push(session);
+    });
+
+    return this.track;
+}
+
+
 // Get the file content and do further processing
 let files = util.path.getRightPath(argv);
 reader.getTalkList(files).then(talkList => {
@@ -31,7 +106,9 @@ reader.getTalkList(files).then(talkList => {
     let talks = util.array.merge(talkList);
     talks = util.talk.str2Obj(talks);
     
-    console.log("--talks--", talks);
+    console.log("--0--", track.generator(0));
+
+    // console.log("--talks--", talks);
 
 }).catch(err => {
     // Handling Errors
